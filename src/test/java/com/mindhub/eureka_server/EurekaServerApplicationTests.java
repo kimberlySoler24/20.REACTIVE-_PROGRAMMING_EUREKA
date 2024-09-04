212c2919f66dd017475fb854f0b2a6db95503f11
PuliAdrea <puli1515@gmail.com>
2024-09-04 17:55:55 -0500

    Se sube proyecto relacionado con la conexión al eureka

---
+++ src/test/java/com/mindhub/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
